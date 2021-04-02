dba2068d5330fa46fa2178518281ba4bb8037c42
Andjela Casic <andjela.casic@gmail.com>
2021-04-02 11:46:23 +0200

    completed chat-form

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss']
+  styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent implements OnInit {
+  @Input() conversation;
+  @Output() onSubmit: EventEmitter<any> = new EventEmitter();
+  emojiPickerVisible;
+  message = '';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  submitMessage(event) {
+    let value = event.target.value.trim();
+    this.message = '';
+    if (value.length < 1) return false;
+    // value.replace(/(.{5})/g, "$1<br>");
+    this.conversation.latestMessage = value;
+    this.conversation.messages.unshift({
+      id: 1,
+      body: value,
+      time: '10:21',
+      me: true,
+    });
   }
 
+  emojiClicked(event) {
+    this.message += event.emoji.native;
+  }
 }
